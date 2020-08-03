2ce18a597436ad1f99d336d657d5afc6ae450be0
Marco Veloso <marco.veloso@bcb.gov.br>
2020-08-03 11:52:30 -0300

    tamanho setado pra celular

--- js/predict.js
+++ js/predict.js
@@ -1,3 +1,28 @@
+TARGET_CLASSES = {
+	0: "10_g1",
+	1: "10_g2",
+	2: "100_g1",
+	3: "100_g2",
+	4: "2_g1",
+	5: "2_g2",
+	6: "20_g1",
+	7: "20_g2",
+	8: "5_g1",
+	9: "5_g2",
+   10: "50_g1",
+   11: "50_g2"
+};
+
+IMAGENS = ["nota2","nota5","nota10","nota20","nota50","nota100"];
+
+let model;
+
+async function load_model() {
+    console.log( "Loading model..." );
+    model = await tf.loadGraphModel('model/model.json');
+    console.log( "Model loaded." );	
+}
+
 async function predict(model, image) {
 	
 	let tensor = tf.browser.fromPixels(image, 3)
@@ -23,7 +48,6 @@ async function predict(model, image) {
 	return top5;
 }
 
-
 async function predict_imagemfixa(model, imageID) {
 	let image = $('#' + imageID).get(0);
 	let list = $("#" + imageID + "_pred");
@@ -32,7 +56,7 @@ async function predict_imagemfixa(model, imageID) {
 
 	list.empty();
 	top5.forEach(function (p) {
-		list.append(`<li>${p.className}: ${p.probability.toFixed(6)}</li>`);
+		list.append(`Predição: <li>${p.className}: ${p.probability.toFixed(6)}</li>`);
 	});	
 }
 
@@ -43,8 +67,8 @@ async function predict_camera(model, image) {
 
 	list.empty();
 	top5.forEach(function (p) {
-		if (p.probability > 0.5)
-			list.append(`<li>${p.className}: ${p.probability.toFixed(6)}</li>`);
+		if (p.probability > 0.3)
+			list.append(`Predição: <li>${p.className}: ${p.probability.toFixed(6)}</li>`);
 	});	
 }
 
@@ -58,16 +82,6 @@ $("#image-selector").change(function () {
 	
 	let file = $("#image-selector").prop('files')[0];
 	reader.readAsDataURL(file);
-});
-
-let model;
-let imagens = ["nota2","nota5","nota10","nota20","nota50","nota100"]
-$( document ).ready(async function () {
-	$('.progress-bar').show();
-    console.log( "Loading model..." );
-    model = await tf.loadGraphModel('model/model.json');
-    console.log( "Model loaded." );
-	$('.progress-bar').hide();
 });
 
 $("#predict-button").click(async function () {
